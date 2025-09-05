15d13acc1aef3bf93a4906ae82389eb905901afd
NickMur <nikitka.murashov@gmail.com>
2025-09-05 16:35:02 +0300

    Перевод аудио в текст с помощью Whisper Openai

---
+++ backend/utilities/consts.py
@@ -0,0 +1,18 @@
+class WhisperModelsENUM:
+    TINY = "tiny"
+    BASE = "base"
+    SMALL = "small"
+    MEDIUM = "medium"
+    LARGE = "large"
+
+class SupportedLanguagesCodesEnum:
+    EN = "en"
+    RU = "ru"
+
+class SupportedExtensionsEnum:
+    MP3 = ".mp3"
+    MP4 = ".mp4"
+    M4A = ".m4a"
+    WAV = ".wav"
+    WEBM = ".webm"
+    OGG = ".ogg"
